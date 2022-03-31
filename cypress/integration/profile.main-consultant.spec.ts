9cbc1c09816726b954e2fc318c96ecf4694fbe13
Jan Amann <jan@amann.me>
2022-03-31 10:30:07 +0200

    fix: resolve todo statements

--- cypress/integration/profile.main-consultant.spec.ts
+++ cypress/integration/profile.main-consultant.spec.ts
@@ -48,15 +48,35 @@ describe('Main consultant', () => {
 		});
 	});
 	describe('Meine Nachrichten', () => {
-		it('should be able to check Asker profile', () => {
+		beforeEach(() => {
 			mainConsultantMockedLogin(3);
+			cy.intercept(`${apiUrl}/service/users/sessions/1/monitoring`, {});
+			cy.intercept(config.endpoints.consultantSessions, {});
+			cy.fixture('services.users.consultants.json').then((consultangs) =>
+				cy.intercept(
+					config.endpoints.agencyConsultants + '?agencyId=1',
+					consultangs
+				)
+			);
+			cy.contains('Meine Nachrichten').click();
+		});
+		it('should be able to check Asker profile', () => {
 			cy.get('.navigation__item ').contains('Meine Nachrichten').click();
 			cy.get('[data-cy=sessions-list-items-wrapper]').click();
 			cy.get('.sessionInfo__username a').click();
 		});
-		// ToDo: Session: Feedback Chat!
 		it('should be able to assign enquiries', () => {
-			cy.get('.select__input__control').contains('Beratung zuweisen');
+			cy.intercept(
+				config.endpoints.sessionBase +
+					'/1/consultant/8c286b12-d6af-419d-9643-fbef9a82db6a',
+				{ statusCode: 200 }
+			);
+
+			cy.get('.sessionsListItem__content').first().click();
+			cy.get('h3:contains("sucht-asker-3")').click();
+			cy.get(".select__inputLabel:contains('Beratung zuweisen')").click();
+			cy.get('.select__option__label:contains("Susanne Müller")').click();
+			cy.contains('Du hast die Beratung erfolgreich zugewiesen.');
 		});
 	});
 	describe('Peer Beratungen', () => {
--- cypress/integration/profile.peer-consultant.spec.ts
+++ cypress/integration/profile.peer-consultant.spec.ts
@@ -1,8 +1,11 @@
 import { generateMultipleConsultantSessions } from '../support/sessions';
 import { config, apiUrl } from '../../src/resources/scripts/config';
 
-const mainConsultantMockedLogin = (amountOfSessions: number) => {
-	const sessions = generateMultipleConsultantSessions(amountOfSessions);
+const peerConsultantMockedLogin = (amountOfSessions: number) => {
+	const sessions = generateMultipleConsultantSessions(amountOfSessions).map(
+		(cur) => ({ ...cur, session: { ...cur.session, isPeerChat: true } })
+	);
+
 	cy.caritasMockedLogin({
 		type: 'peer-consultant',
 		sessions
@@ -28,8 +31,7 @@ describe('Peer consultant', () => {
 			}
 		);
 
-		mainConsultantMockedLogin(3);
-		cy.get('.navigation__item').contains('Profil').click();
+		peerConsultantMockedLogin(3);
 	});
 
 	it('should show Meine Nachrichten, Profil, Erstanfragen', () => {
@@ -37,6 +39,7 @@ describe('Peer consultant', () => {
 		cy.get('.navigation__item').contains('Erstanfragen');
 		cy.get('.navigation__item').contains('Profil');
 	});
+
 	describe('Erstanfragen', () => {
 		it('should check Erstanfragen and Live-Chat Anfragen', () => {
 			cy.intercept(config.endpoints.consultantEnquiriesBase, {});
@@ -44,17 +47,35 @@ describe('Peer consultant', () => {
 			cy.contains('Live-Chat Anfragen').click();
 		});
 	});
+
 	describe('Meine Nachrichten', () => {
+		beforeEach(() => {
+			cy.intercept(`${apiUrl}/service/users/sessions/1/monitoring`, {});
+			cy.intercept(config.endpoints.consultantSessions, {});
+			cy.fixture('services.users.consultants.json').then((consultangs) =>
+				cy.intercept(
+					config.endpoints.agencyConsultants + '?agencyId=1',
+					consultangs
+				)
+			);
+			cy.contains('Meine Nachrichten').click();
+		});
 		it('should be able to check Asker profile', () => {
-			cy.get('.navigation__item').contains('Meine Nachrichten').click();
 			cy.get('[data-cy=sessions-list-items-wrapper]').click();
 			cy.get('.sessionInfo__username a').click();
 		});
-		// ToDo: Session: Feedback Chat!
 		it('should not be able to assign enquiries', () => {
-			cy.get('.select__input__control')
-				.contains('Beratung zuweisen')
-				.should('not.exist');
+			cy.intercept(
+				config.endpoints.sessionBase +
+					'/1/consultant/8c286b12-d6af-419d-9643-fbef9a82db6a',
+				{ statusCode: 200 }
+			);
+
+			cy.get('.sessionsListItem__content').first().click();
+			cy.get('h3:contains("sucht-asker-3")').click();
+			cy.get(".select__inputLabel:contains('Beratung zuweisen')").should(
+				'not.exist'
+			);
 		});
 	});
 });
