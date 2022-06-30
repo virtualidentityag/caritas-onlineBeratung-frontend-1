2308785061e03b8c1e508165fb6f018a9a5d8ee6
aalicic <adnan@leads.ba>
2022-06-30 16:01:43 +0200

    feat: merge from OS

--- src/components/messageSubmitInterface/messageSubmitInterfaceComponent.tsx
+++ src/components/messageSubmitInterface/messageSubmitInterfaceComponent.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { SendMessageButton } from './SendMessageButton';
 import {
 	getChatItemForSession,
@@ -149,7 +149,7 @@ const SAVE_DRAFT_TIMEOUT = 10000;
 
 export interface MessageSubmitInterfaceComponentProps {
 	className?: string;
-	handleSendButton: Function;
+	handleSendButton?: Function;
 	isTyping?: Function;
 	placeholder: string;
 	showMonitoringButton?: Function;
@@ -158,6 +158,8 @@ export interface MessageSubmitInterfaceComponentProps {
 	sessionIdFromParam?: number;
 	groupIdFromParam?: string;
 	language?: string;
+	preselectedFile?: File;
+	handleMessageSendSuccess?: Function;
 }
 
 export const MessageSubmitInterfaceComponent = (
@@ -496,7 +498,8 @@ export const MessageSubmitInterfaceComponent = (
 
 	const sendMessage = () => {
 		const attachmentInput: any = attachmentInputRef.current;
-		const attachment = attachmentInput && attachmentInput.files[0];
+		const selectedFile = attachmentInput && attachmentInput.files[0];
+		const attachment = props.preselectedFile || selectedFile;
 		if (getTypedMarkdownMessage() || attachment) {
 			setIsRequestInProgress(true);
 		} else {
@@ -517,7 +520,7 @@ export const MessageSubmitInterfaceComponent = (
 			)
 				.then((response) => {
 					setEditorState(EditorState.createEmpty());
-					props.handleSendButton(response);
+					props.handleSendButton?.(response);
 				})
 				.catch((error) => {
 					console.log(error);
@@ -564,6 +567,7 @@ export const MessageSubmitInterfaceComponent = (
 	};
 
 	const handleMessageSendSuccess = () => {
+		props.handleMessageSendSuccess?.();
 		if (props.showMonitoringButton) {
 			props.showMonitoringButton();
 		}
@@ -604,6 +608,16 @@ export const MessageSubmitInterfaceComponent = (
 		attachmentInput.click();
 	};
 
+	const displayAttachmentToUpload = (attachment: File) => {
+		setAttachmentSelected(attachment);
+		setActiveInfo('');
+	};
+
+	const handleLargeAttachments = useCallback(() => {
+		removeSelectedAttachment();
+		setActiveInfo(INFO_TYPES.ATTACHMENT_SIZE_ERROR);
+	}, []);
+
 	const handleAttachmentChange = () => {
 		const attachmentInput: any = attachmentInputRef.current;
 		const attachment = attachmentInput.files[0];
@@ -613,15 +627,18 @@ export const MessageSubmitInterfaceComponent = (
 			: displayAttachmentToUpload(attachment);
 	};
 
-	const displayAttachmentToUpload = (attachment: File) => {
-		setAttachmentSelected(attachment);
-		setActiveInfo('');
-	};
+	const handlePreselectedAttachmentChange = useCallback(() => {
+		const attachment = props.preselectedFile;
+		const attachmentSizeMB = getAttachmentSizeMBForKB(attachment.size);
+		attachmentSizeMB > ATTACHMENT_MAX_SIZE_IN_MB
+			? handleLargeAttachments()
+			: displayAttachmentToUpload(attachment);
+	}, [handleLargeAttachments, props.preselectedFile]);
 
-	const handleLargeAttachments = () => {
-		removeSelectedAttachment();
-		setActiveInfo(INFO_TYPES.ATTACHMENT_SIZE_ERROR);
-	};
+	useEffect(() => {
+		if (!props.preselectedFile) return;
+		handlePreselectedAttachmentChange();
+	}, [handlePreselectedAttachmentChange, props.preselectedFile]);
 
 	const removeSelectedAttachment = () => {
 		const attachmentInput: any = attachmentInputRef.current;
@@ -652,9 +669,10 @@ export const MessageSubmitInterfaceComponent = (
 		if (activeInfo === INFO_TYPES.ABSENT) {
 			infoData = {
 				isInfo: true,
-				infoHeadline:
-					translate('consultant.absent.message') +
-					getContact(activeSession).username,
+				infoHeadline: `${
+					getContact(activeSession).displayName ||
+					getContact(activeSession).username
+				} ${translate('consultant.absent.message')} `,
 				infoMessage: activeSession.consultant.absenceMessage
 			};
 		} else if (activeInfo === INFO_TYPES.ATTACHMENT_SIZE_ERROR) {
