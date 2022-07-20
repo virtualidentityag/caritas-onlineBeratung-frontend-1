c161d7c45dfabf4fee632cbc81fcd29e3b0cd8eb
sergiofv <sfvsound@gmail.com>
2022-07-20 14:50:49 +0200

    feat: videoTermine page enabled for frontend (not saas-theme)

--- src/resources/scripts/config.ts
+++ src/resources/scripts/config.ts
@@ -17,7 +17,7 @@ export const APP_PATH = 'app';
 export const config = {
 	enableTenantTheming: false, // Feature flag to enable tenant theming based on subdomains
 	enableWalkthrough: true, // Feature flag to enable walkthrough (false by default here & true in the theme repo)
-	enableVideoAppointments: false, // Feature flag to enable Video-Termine page
+	enableVideoAppointments: true, // Feature flag to enable Video-Termine page
 
 	endpoints: {
 		agencyConsultants: apiUrl + '/service/users/consultants',
