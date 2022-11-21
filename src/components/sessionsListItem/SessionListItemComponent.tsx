86c0cf5c6bf61260b0710e17b107992183a8bc35
Carlos Soares <carlos.soares17@gmail.com>
2022-11-21 11:52:17 +0000

    fix: remove consulting type when topic are enabled

--- src/components/sessionsListItem/SessionListItemComponent.tsx
+++ src/components/sessionsListItem/SessionListItemComponent.tsx
@@ -19,10 +19,10 @@ import {
 	hasUserAuthority,
 	SessionTypeContext,
 	STATUS_FINISHED,
-	TenantContext,
 	TopicSessionInterface,
 	useConsultingType,
-	UserDataContext
+	UserDataContext,
+	useTenant
 } from '../../globalState';
 import { getGroupChatDate } from '../session/sessionDateHelpers';
 import { markdownToDraft } from 'markdown-draft-js';
@@ -53,6 +53,7 @@ export const SessionListItemComponent = ({
 	defaultLanguage
 }: SessionListItemProps) => {
 	const { t: translate } = useTranslation(['common', 'consultingTypes']);
+	const tenantData = useTenant();
 	const { sessionId, rcGroupId: groupIdFromParam } =
 		useParams<{ rcGroupId: string; sessionId: string }>();
 	const sessionIdFromParam = sessionId ? parseInt(sessionId) : null;
@@ -64,7 +65,6 @@ export const SessionListItemComponent = ({
 	const { userData } = useContext(UserDataContext);
 	const { type, path: listPath } = useContext(SessionTypeContext);
 	const { isE2eeEnabled } = useContext(E2EEContext);
-	const { tenant } = useContext(TenantContext);
 
 	// Is List Item active
 	const isChatActive =
@@ -301,7 +301,9 @@ export const SessionListItemComponent = ({
 		sessionTopic = session.user.username;
 	}
 
-	const zipCodeSlash = consultingType ? '/ ' : '';
+	const showConsultingType =
+		consultingType && !tenantData?.settings?.featureTopicsEnabled;
+	const zipCodeSlash = showConsultingType ? '/ ' : '';
 
 	return (
 		<div
@@ -329,7 +331,7 @@ export const SessionListItemComponent = ({
 						</div>
 					) : (
 						<div className="sessionsListItem__consultingType">
-							{consultingType
+							{showConsultingType
 								? translate(
 										[
 											`consultingType.${consultingType.id}.titles.default`,
@@ -340,7 +342,8 @@ export const SessionListItemComponent = ({
 								: ''}
 							{session.item.consultingType !== 1 &&
 							!isAsker &&
-							!session.isLive
+							!session.isLive &&
+							!consultingType.registration.autoSelectPostcode
 								? zipCodeSlash + session.item.postcode
 								: null}
 						</div>
@@ -349,7 +352,8 @@ export const SessionListItemComponent = ({
 						<div
 							className="sessionsListItem__topic"
 							style={{
-								backgroundColor: tenant?.theming?.primaryColor
+								backgroundColor:
+									tenantData?.theming?.primaryColor
 							}}
 						>
 							{topicSession?.name}
